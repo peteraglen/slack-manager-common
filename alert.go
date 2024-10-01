3a65bd02ac7610ff3e16552a8b3c9b468efb67ce
Peter Aglen <peteraglen@gmail.com>
2024-10-01 14:02:54 +0200

    Update alert

--- alert.go
+++ alert.go
@@ -3,7 +3,6 @@ package common
 import (
 	"crypto/sha256"
 	"encoding/base64"
-	"encoding/json"
 	"errors"
 	"fmt"
 	"net/url"
@@ -266,11 +265,6 @@ func NewAlert(severity AlertSeverity) *Alert {
 // DedupID returns the ID of the Alert (for database/storage purposes)
 func (a *Alert) DedupID() string {
 	return hash("alert", a.SlackChannelID, a.RouteKey, a.CorrelationID, a.Timestamp.Format(time.RFC3339Nano), a.Header, a.Text)
-}
-
-// TimestampStr returns the timestamp of the alert as a string in RFC3339Nano format
-func (a *Alert) TimestampStr() string {
-	return a.Timestamp.Format(time.RFC3339Nano)
 }
 
 func (a *Alert) Clean() {
@@ -754,16 +748,6 @@ func (a *Alert) ValidateEscalation() error {
 	}
 
 	return nil
-}
-
-func (a *Alert) MarshalJSON() ([]byte, error) {
-	type Alias Alert
-
-	return json.Marshal(&struct {
-		*Alias
-	}{
-		Alias: (*Alias)(a),
-	})
 }
 
 func shortenAlertTextIfNeeded(text string) string {
