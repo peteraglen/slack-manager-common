feec709c5007354268e3b19d9d3ffef63ff36245
Peter Aglen <peteraglen@gmail.com>
2025-07-08 10:58:04 +0200

    Update move mapping interface

--- database_models.go
+++ database_models.go
@@ -11,7 +11,7 @@ type Issue interface {
 
 	// GetCorrelationID returns the correlation ID for this issue.
 	// The correlation ID is used to group related alerts together, and may or may not be client defined.
-	// It is not guaranteed to be unique across all issues, and *cannot* be used as a unique issue identifier.
+	// It is not guaranteed to be unique across all issues, even in the same channel, and *cannot* be used as a unique issue identifier.
 	// It is not URL safe, and should thus be encoded before being used in URLs or as part of a database key.
 	GetCorrelationID() string
 
@@ -31,4 +31,9 @@ type MoveMapping interface {
 	// UniqueID returns a unique and deterministic ID for this move mapping, for database/storage purposes.
 	// The ID is based on certain move mapping fields, and is base64 encoded to ensure it is safe for use in URLs and as a database key.
 	UniqueID() string
+
+	// GetCorrelationID returns the correlation ID that this move mapping is associated with.
+	// It is unique in a single channel, but not across all channels.
+	// It is not URL safe, and should thus be encoded before being used in URLs or as part of a database key.
+	GetCorrelationID() string
 }
